9094e9acdea5e03b1e076e81ebe89ac2396ac13c
artpil <artpil@mail.ru>
2025-12-31 10:24:38 +0400

    add

--- js/core.js
+++ js/core.js
@@ -59,6 +59,32 @@ $(document).ready(function() {
     }
   });
 
+
+  $('.about__slider').owlCarousel({
+    center: false,
+    items: 4,
+    autoWidth: false,
+    loop: false,
+    margin: 40,
+    dots: false,
+    nav: true,
+    navText: [
+      `<img src="./img/sld-prev.svg">`,
+      `<img src="./img/sld-next.svg">`
+    ],
+    responsive: {
+      1200: {
+        items: 4
+      },
+      768: {
+        items: 2,
+      },
+      320: {
+        items: 1.05,
+      }
+    },
+  });
+
   $('#phone-mask').mask('+7(999)999-99-99', {
     placeholder: "+7(___)___-__-__"
   });
